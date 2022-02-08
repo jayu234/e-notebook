1e37dbd0b895650ccf8fb4032df631a0fe6dfa14
jayu234 <84456747+jayu234@users.noreply.github.com>
2022-02-09 00:47:10 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -12,8 +12,10 @@ function About() {
     <h2>About eNotebook</h2><hr/>
     <br/>
     <p>eNotebook is a cloud service plateform where you can store your daily-life notes for completely free of cost.</p>
-    <p>eNotebook is created using MERN stack. We used various technologies like <a href="https://getbootstrap.com/">Bootstrap 5</a>(for frontend dedigning), <a href="https://www.npmjs.com/">NPM</a>(as package manager), <a href="https://github.com">GitHub</a>(for version control) and <a href="https://fontawesome.com/v5.15/icons">Fontawesome</a>(for icons).</p>
-    <p>Complete source code of eNotebook is available here.</p>
+    <p>eNotebook is created using MERN stack. We used various technologies like <a href="https://getbootstrap.com/">Bootstrap 5</a>(for frontend designing), <a href="https://www.npmjs.com/">NPM</a>(as package manager), <a href="https://github.com">GitHub</a>(for version control) and <a href="https://fontawesome.com/v5.15/icons">Fontawesome</a>(for icons).</p>
+    <p>Git repositories:</p>
+    <p><a href="https://github.com/jayu234/eNotebook-frontend-MERN-Project-1">Frontend</a></p>
+    <p><a href="https://github.com/jayu234/eNotebook-backend-MERN-Project-1">Backend</a></p>
   </div>
   </>
   );
